f1c8e33a04569752ac5498a8b98ea10fcd4085a5
Devin Walters <devinw@gmail.com>
2010-03-31 22:04:44 -0500

    Updated the clojure, contrib, and swank-clojure dependencies to be 1.2.0 snapshot versions.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject walton "0.5"
+(defproject walton "0.5.2"
   :description "Walton will traverse caves filled with snakes to find you example clojure code containing the function you desire."
-  :dependencies [[org.clojure/clojure "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
 		 [clj-sandbox/clj-sandbox "0.2.10-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [clj-html "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
                      [autodoc "0.7.0"]]
