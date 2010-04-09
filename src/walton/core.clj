9501104fe2bb256d91010d6cfde22b8a2fd7e714
Devin Walters <devinw@gmail.com>
2010-04-09 02:19:48 -0500

    fixed some issues with syntax highlighting, added function which will check to see if your query is a valid clojure.core function and if so, will print the docstring at the top of the page.  i removed syntax hilighting for results.  this makes things more readable.

--- src/walton/core.clj
+++ src/walton/core.clj
@@ -4,7 +4,7 @@
    [hiccup core page-helpers form-helpers]
    net.licenser.sandbox
    ring.util.response
-   [walton layout web integration])
+   [walton integration layout web irc])
   (:gen-class))
 
 (def *sandbox* (stringify-sandbox (new-sandbox-compiler :timeout 100)))
--- src/walton/layout.clj
+++ src/walton/layout.clj
@@ -1,28 +1,52 @@
 (ns walton.layout
-  (:use [hiccup core page-helpers form-helpers]
-        walton.core
-        walton.web))
+  (:use [hiccup core page-helpers form-helpers]))
+
+(defn unveil-namespace
+  "Gets all of the vals in the map produced by (ns-publics 'ns)"
+  [ns]
+  (vals (ns-publics ns)))
+
+(defn format-docstring
+  "Formats the docstring for the web."
+  [v]
+  (html
+   [:blockquote [:i (str (ns-name (:ns (meta v))) "/" (:name (meta v)))]
+    [:br]
+    [:pre.brush:.clojure (str (:arglists (meta v)))]
+    (when (:macro (meta v))
+      [:b "Macro"])
+    [:p (str (:doc (meta v)))]]))
 
 (defn application [text body]
-     (html
-      [:html
-       [:head
-        (include-js "/javascript/syntaxhilighter/scripts/shCore.js"
-                    "/javascript/syntaxhilighter/scripts/shBrushClojure.js")
-        (include-css "/javascript/syntaxhilighter/styles/shCore.css"
-                     "/javascript/syntaxhilighter/styles/shThemeDefault.css")
-        (javascript-tag "SyntaxHighlighter.all();")
-        [:title text]]
-       [:body
-        [:h3 text]
-        body]]))
+  (html
+   [:html
+    [:head
+     (include-js "/javascript/syntaxhilighter/scripts/shCore.js"
+                 "/javascript/syntaxhilighter/scripts/shBrushClojure.js")
+     (include-css "/javascript/syntaxhilighter/styles/shCore.css"
+                  "/javascript/syntaxhilighter/styles/shThemeDefault.css")
+     [:script {:type "text/javascript"}
+"//<![CDATA[
+SyntaxHighlighter.defaults['gutter'] = false;
+SyntaxHighlighter.defaults['toolbar'] = false;
+SyntaxHighlighter.defaults['light'] = true;
+SyntaxHighlighter.all();
+//]]>"]
+     [:title text]]
+    [:body
+     [:h1 text]
+     (let [sym (-> (str "clojure.core/" text) symbol find-var)]
+       (if (meta sym)
+         (format-docstring sym)))
+     body]]))
 
 (defn code-list [body]
-     (html
-      [:ul
-       body]))
+  (html
+   [:ul
+    body]))
 
 (defn code-block [[code result]]
-     (html
-      [:li
-       [:pre.brush:.clojure code] [:pre.brush:.clojure result]]))
+  (html
+   [:li
+    [:pre.brush:.clojure code]
+    [:pre.brush:.clojure (str "; =&gt ") (str result)]]))
