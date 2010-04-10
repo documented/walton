8c1f18780315b37630b836d2b7a224eac1aa758a
Devin Walters <devinw@gmail.com>
2010-04-10 05:54:10 -0500

    now with distinct results, stole some labrepl code, and worked on getting some fancy JS show/hide, also fixed up the truncate function and did some maintenance

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject walton "0.5.4"
   :description "Walton will traverse caves filled with snakes to find you example clojure code containing the function you desire."
-  :url "http://getclojure.org/"
+  :url "http://getclojure.org:8080/examples/concat"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [clj-sandbox/clj-sandbox "0.2.12"]
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+                 [clj-sandbox/clj-sandbox "0.2.13"]
+                 [ring/ring-jetty-adapter "0.2.0"]
+                 [ring "0.2.0"]
                  [hiccup "0.2.3"]
                  [net.cgrand/moustache "1.0.0-SNAPSHOT"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [ring/ring-jetty-adapter "0.2.0"]
-                 [ring "0.2.0"]]
+                 [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.2.0-SNAPSHOT"]
                      [autodoc "0.7.0"]]
--- src/walton/layout.clj
+++ src/walton/layout.clj
@@ -1,5 +1,7 @@
 (ns walton.layout
-  (:use [hiccup core page-helpers form-helpers]))
+  (:use [hiccup core page-helpers form-helpers]
+        [clojure.contrib pprint with-ns])
+  (:require [clojure.contrib.str-utils2 :as s]))
 
 (defn unveil-namespace
   "Gets all of the vals in the map produced by (ns-publics 'ns)"
@@ -17,21 +19,57 @@
       [:b "Macro"])
     [:p (str (:doc (meta v)))]]))
 
+(defn format-code
+  [& codes]
+  (apply str (map
+              (fn [code]
+                (if (string? code)
+                  (str code "\n")
+                  (with-out-str (pprint code))))
+              codes)))
+
+(defn one-liner?
+  [s]
+  (if s
+    (< (count (remove empty? (s/split s #"\s*\n\s*"))) 2)
+    true))
+
+(defn code*
+  "Show codes (literal strings or forms) in a pre/code block."
+  [& codes]
+  (let [code-string (apply format-code codes)
+        class-string "brush: clojure; toolbar: false;"
+        class-string (if (one-liner? code-string) (str class-string  " light: true;") class-string)]
+    [:script {:type "syntaxhighlighter" :class class-string}
+     (str "<![CDATA[" code-string "]]>")]))
+
+(defn hide-show [#^String s]
+  [:div {:class "toggle"}
+   [:div [:a {:href "javascript:void(null)"} "Show Code"]]
+   [:div {:style "display:none;"} [:a {:href "javascript:void(null)"} "Hide Code"]
+    (code* s)]])
+
+(defn google-api []
+  (html
+   (include-js "http://www.google.com/jsapi?key=ABQIAAAAf81JAvR-zjjyFg4UPBcQ7hRUEEC_1sQajlg8OegeKNzx-mGf0RTW79MfTS7T6otdVJ5Q6A0_gqwzcg")
+   (javascript-tag "google.load(\"jquery\", \"1.4.2\");
+google.load(\"jqueryui\", \"1.8.0\");")))
+
 (defn application [text body]
   (html
    [:html
     [:head
      (include-js "/javascript/syntaxhilighter/scripts/shCore.js"
-                 "/javascript/syntaxhilighter/scripts/shBrushClojure.js")
+                 "/javascript/syntaxhilighter/scripts/shBrushClojure.js"
+                 "/javascript/jquery-1.4.2.js"
+                 "/javascript/application.js")
      (include-css "/javascript/syntaxhilighter/styles/shCore.css"
-                  "/javascript/syntaxhilighter/styles/shThemeDefault.css")
-     [:script {:type "text/javascript"}
-"//<![CDATA[
-SyntaxHighlighter.defaults['gutter'] = false;
+                  "/javascript/syntaxhilighter/styles/shThemeDefault.css"
+                  "/css/application.css")
+     (javascript-tag "SyntaxHighlighter.defaults['gutter'] = false;
 SyntaxHighlighter.defaults['toolbar'] = false;
 SyntaxHighlighter.defaults['light'] = true;
-SyntaxHighlighter.all();
-//]]>"]
+SyntaxHighlighter.all();" )
      [:title text]]
     [:body
      [:h1 text]
